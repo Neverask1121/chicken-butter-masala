68324dec3b571d115cfae940599b77507a064a90
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-21 14:20:22 +0530

    updating widgets.py

--- 9-streamlit/widgets.py
+++ 9-streamlit/widgets.py
@@ -5,5 +5,14 @@ import streamlit as st
 st.title("StramLit text input")
 
 name=st.text_input("Enter your name")
+
+age=st.slider("Select your age:", 0, 100, 25)
+
+st.write(f"Your age is something like this {age}")
+
+options = ["Python", "Java", "CPP", "Javascript"]
+choice = st.selectbox("Choose you favourite language", options)
+st.write(f"Nigga selected: {choice}")
+
 if name:
   st.write(f"Hello, {name}")
